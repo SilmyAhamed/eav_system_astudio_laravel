91a118b5eabdfea556437dd940bdd7ef2b2be8ba
SilmyAhamed <silmypc@gmail.com>
2025-02-10 21:30:13 +0400

    Model relation added

--- app/Models/Timesheet.php
+++ app/Models/Timesheet.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Timesheet extends Model
 {
-    //
+    protected $fillable = ['user_id', 'project_id', 'task_name', 'date', 'hours'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+    }
+
+    public function timesheets()
+    {
+        return $this->hasMany(Timesheet::class);
+    }
 }
