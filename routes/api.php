54aaedfd97bd59677b435b5b71dd74201e268662
SilmyAhamed <silmypc@gmail.com>
2025-02-10 17:37:05 +0400

    Passport initiated

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,48 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+// =================================
+use Laravel\Passport\Http\Controllers\AuthorizationController;
+use Laravel\Passport\Http\Controllers\AccessTokenController;
+use Laravel\Passport\Http\Controllers\TransientTokenController;
+use Laravel\Passport\Http\Controllers\ClientController;
+use Laravel\Passport\Http\Controllers\PersonalAccessTokenController;
+// =================================
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API is working']);
 });
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+
+
+
+
+
+
+// =================================
+Route::post('/oauth/token', [AccessTokenController::class, 'issueToken'])
+    ->middleware(['throttle']);
+
+Route::post('/oauth/authorize', [AuthorizationController::class, 'authorize']);
+
+Route::post('/oauth/tokens', [TransientTokenController::class, 'index']);
+
+Route::post('/oauth/clients', [ClientController::class, 'store']);
+Route::get('/oauth/clients', [ClientController::class, 'index']);
+Route::delete('/oauth/clients/{client_id}', [ClientController::class, 'destroy']);
+
+Route::post('/oauth/personal-access-tokens', [PersonalAccessTokenController::class, 'store']);
+Route::get('/oauth/personal-access-tokens', [PersonalAccessTokenController::class, 'index']);
+Route::delete('/oauth/personal-access-tokens/{token_id}', [PersonalAccessTokenController::class, 'destroy']);
+// =================================
